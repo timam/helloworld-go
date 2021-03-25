ade4e757e449957ab9a9f24719189d6ef4a2106c
Timam <mail@timam.io>
2021-03-25 21:41:28 +0600

    cloudwatch log send

---
+++ 00-Curl/go.mod
@@ -0,0 +1,5 @@
+module github.com/timam/helloworld-go
+
+go 1.15
+
+require github.com/aws/aws-sdk-go v1.38.3
--- 00-Curl/main.go
+++ 00-Curl/main.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatch"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"strconv"
+	"time"
 )
 
+
+const statusUp  = 1.0
+const statusDown = 0.0
+
+//https://admin.uatsm.bka.sh/admin/actuator/health
+//https://callback.uatsm.bka.sh/cps/actuator/health
+//https://callback.uatsm.bka.sh/processor/actuator/health/
+//https://admin-app.uatsm.bka.sh/admin-app/actuator/health
+
 var method = "GET"
-var endpoint = "https://eventstream.uatcapp.bka.sh/event-stream/actuator/health"
+var endpoint = "https://eventstream.uatcapp.bka.sh/event-stream/actuator/health/"
+var serviceName = "mysteram"
+
 
 func gimmeResponse(method, endpoint string) (int, string) {
 
@@ -26,7 +43,57 @@ func gimmeResponse(method, endpoint string) (int, string) {
 	return response.StatusCode, string(body)
 }
 
+func putStatusCodeToCloudwatch(status float64, serviceName string, httpStatus string) {
+
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create new cloudwatch client.
+	svc := cloudwatch.New(sess)
+
+	_, err := svc.PutMetricData(&cloudwatch.PutMetricDataInput{
+		Namespace: aws.String("EKS-UPTIME"),
+		MetricData: []*cloudwatch.MetricDatum{
+			&cloudwatch.MetricDatum{
+				MetricName: aws.String("Status"),
+				Unit:       aws.String("Count"),
+				Value:      aws.Float64(status),
+				Dimensions: []*cloudwatch.Dimension{
+					&cloudwatch.Dimension{
+						Name:  aws.String("ServiceName"),
+						Value: aws.String(serviceName),
+					},
+					&cloudwatch.Dimension{
+						Name:  aws.String("StatusCode"),
+						Value: aws.String(httpStatus),
+					},
+				},
+			},
+		},
+	})
+
+	if err != nil{
+		fmt.Println("Cloudwatch Put Error :", err)
+	}
+
+}
+
+
 func main() {
-	statusCode, body := gimmeResponse(method, endpoint)
-	fmt.Println(statusCode, body)
+
+	for {
+		statusCode, body := gimmeResponse(method, endpoint)
+		strStatusCode := strconv.Itoa(statusCode)
+
+		if statusCode == 200{
+			putStatusCodeToCloudwatch(statusUp, serviceName, strStatusCode)
+			log.Println(fmt.Sprintf("%s is: up, status code:  %s, body: %s", serviceName, strStatusCode, body))
+		} else {
+			putStatusCodeToCloudwatch(statusDown, serviceName, strStatusCode)
+			log.Println(fmt.Sprintf("%s is: unhealthy, status code:  %s", serviceName, strStatusCode))
+		}
+		time.Sleep(1000 *time.Millisecond)
+	}
+
 }
