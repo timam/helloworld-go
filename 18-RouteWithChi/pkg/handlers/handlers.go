475516820250d369c59426981eed043225079770
Timam <mail@timam.io>
2021-04-02 20:58:35 +0600

    chi router added

---
+++ 18-RouteWithChi/pkg/handlers/handlers.go
@@ -0,0 +1,45 @@
+package handlers
+
+import (
+	"github.com/timam/timam/pkg/config"
+	"github.com/timam/timam/pkg/modles"
+	"github.com/timam/timam/pkg/render"
+	"net/http"
+)
+
+//Repo the repository used by the handlers
+var Repo *Repository
+
+//Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+//NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository  {
+	return &Repository{
+		App : a,
+	}
+}
+
+//NewHandlers sets the repository for handlers
+func NewHandlers(r *Repository)  {
+	Repo = r
+}
+
+//Home is the home page handlers
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.gohtml", &modles.TemplateData{})
+}
+
+//About is the about page handlers
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello Again"
+
+	render.RenderTemplate(w, "about.page.gohtml", &modles.TemplateData{
+		StringMap: stringMap,
+	})
+}
+
